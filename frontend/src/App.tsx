6bd951a2e0677740123f015310fc5dac8c5a446d
Eliot Lim <eliot_LIM@tech.gov.sg>
2021-09-07 15:18:31 +0800

    Add TodoApp draft and React DOM routing
    + Navigation Bar / Router switches between index `/` and todo `/todo` screens
    + Simple Todo list and state management
    + SGDS and dependencies installed: `sgds-govtech`, `styled-components`, 'sgds-govtech-react`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Clock from './components/Clock';
+import NavigationBar from './components/NavigationBar';
+import Todo from './screens/Todo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-        <Clock interval={1000}></Clock>
-      </header>
-    </div>
+    <Router>
+      <NavigationBar />
+      <Switch>
+        <Route path="/todo">
+          <Todo></Todo>
+        </Route>
+        <Route path="/">
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+              </a>
+              <Clock interval={1000}></Clock>
+            </header>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
