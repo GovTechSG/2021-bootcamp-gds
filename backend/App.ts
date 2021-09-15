003fbe63ebb664bf9de6fcaede8a591075ce809a
lucas_yap <yapyuyou@hotmail.com>
2021-09-15 19:53:24 +0800

    add self-hosted swagger docs

--- backend/App.ts
+++ backend/App.ts
@@ -1,9 +1,13 @@
 import express from "express";
 
 import TodoRouter from "./src/routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerJson from "./swagger.json";
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
