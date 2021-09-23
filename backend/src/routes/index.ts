65e6c616218bf55e0ed4a53095eae6dc01a37d96
Oliver Chan <oliverchanyw@gmail.com>
2021-09-23 16:53:35 +0800

    checkpoint 2 working backwards from checkpoint 3

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,8 +4,7 @@ import {
   getAllTodos,
   deleteTodoById,
   getTodoById, 
-  updateTodoById,
-  createRandomTodo
+  updateTodoById
 } from "./methods";
 
 const todoRouter = Router();
@@ -14,7 +13,6 @@ todoRouter.get("/todos", getAllTodos);
 todoRouter.get("/todos/:id", getTodoById);
 todoRouter.put("/todos/:id", updateTodoById);
 todoRouter.delete("/todos/:id", deleteTodoById);
-todoRouter.post("/todos/random", createRandomTodo);
 
 // For a demo, can ignore for now
 todoRouter.get("/demo/time", (req, res) => res.status(200).send({ time: new Date().toTimeString() }));
