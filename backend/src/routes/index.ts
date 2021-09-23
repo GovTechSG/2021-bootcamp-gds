680f5aaaac835f8ed85b782bfb6e017fe2194add
Oliver Chan <oliverchanyw@gmail.com>
2021-09-23 17:00:38 +0800

    removed demo/time

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -13,7 +13,4 @@ todoRouter.get("/todos/:id", getTodoById);
 todoRouter.put("/todos/:id", updateTodoById);
 todoRouter.delete("/todos/:id", deleteTodoById);
 
-// For a demo, can ignore for now
-todoRouter.get("/demo/time", (req, res) => res.status(200).send({ time: new Date().toTimeString() }));
-
 export default todoRouter;
