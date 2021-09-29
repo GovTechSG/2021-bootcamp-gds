a5b5e28fa74e6efb6f05732590a5d9d85c8e74df
Darryl Sw <darryl_sw@hive.gov.sg>
2021-09-29 17:34:55 +0800

    Add CORS all

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -17,11 +17,6 @@ todoRouter.delete("/todos/:id", deleteTodoById);
 todoRouter.post("/todos/random", createRandomTodo);
 
 todoRouter.get("/demo/time", (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, DELETE');
-  res.set('Access-Control-Allow-Credentials', 'true');
-  res.set('Access-Control-Allow-Headers', '*');
-  res.set('Access-Control-Max-Age', '3600');
   res.status(200).send({ time: new Date().toTimeString() })
 });
 
